42673875e4ec40fa4f8f433ada96f4fdf6a0582d
bhargav gajera <vishgajera@gmail.com>
2023-11-28 23:30:29 +0530

    adding redux and axios

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Trivia from './Components/trivia/Trivia'
+import Trivia from './components/trivia/Trivia'
 
 function AppRoutes() {
   return (
--- src/Components/trivia/Trivia.js
+++ src/Components/trivia/Trivia.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { fetchQuiz } from "../../store/quizSlice";
+import { useSelector,useDispatch } from "react-redux";
 
 function Trivia() {
+
+  const dispatch = useDispatch();
+  const {quiz,isLoaded,error} = useSelector(state=>state.quiz)
+
+  console.log({quiz,isLoaded,error});
+
+  useEffect(() => {
+    dispatch(fetchQuiz())
+    return () => {
+    }
+  }, [dispatch])
+  
+
   return (
+
     <section className="quiz">
       <p className="correct-answers">correct answers:0/0</p>
       <article className="container">
-        <h2>
+
+        {isLoaded &&
+          <h2>
+          {quiz.results[0].question}
+          </h2>
+        }
+        {!isLoaded &&
+          <h2 >
           Which one of these superhero teams appears in the Invincible comics?
-        </h2>
+          </h2>
+        }
+
         <div className="btn-container">
           <button className="answer-btn">Avengers</button>
           <button className="answer-btn">Justice League</button>
